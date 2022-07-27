aa10fccf60a8d5547410e506f4a055da36d54ece
Lucien Shaw <myxlc55@outlook.com>
2022-07-27 16:59:17 +0800

    更改自毁程序位置

--- windows/maintain.bat
+++ windows/maintain.bat
@@ -1,6 +1,6 @@
 @echo off
 :loop
-if not exist "C:\Users\root\remove_me_for_self_destruct" (
+if not exist "D:\remove_me_for_self_destruct" (
     echo "Initiating self-destruct sequence"
     goto end_of_loop
 )
